f4f406992779d9663a70b0d7d92dd56091bbfedb
Yannick Dorn <yannick.dorn@gmail.com>
2019-12-04 00:29:50 -0500

    sending to pi

--- humidity.py
+++ humidity.py
@@ -1,13 +1,25 @@
 import Adafruit_DHT
+from flask import Flask, escape, request
+
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 
 DHT_PIN = 4
 
 
-while True :
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity))
-    else :
-        print("Failed to retrieve data from humidity sensor")
+@app.rout('/')
+def hello() :
+    # while True :
+      humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+      if humidity is not None and temperature is not None:
+          ("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity))
+      else :
+          print("Failed to retrieve data from humidity sensor")
+# from flask import Flask, escape, request
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello() :
+#     name = request.args.get("Name", "World")
+#     return 'Hello World'
