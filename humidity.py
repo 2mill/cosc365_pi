057849a965da23834c0db42a467d0fc6ed408594
Yannick Dorn <yannick.dorn@gmail.com>
2019-12-04 20:32:41 -0500

    m

--- humidity.py
+++ humidity.py
@@ -6,6 +6,8 @@ DHT_PIN = 4
 def returnTemp() :
     humidity,temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidtiy))
+        return humidity, temperature;
+        # return ("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidtiy))
     else :
-        print("Failed to retrive data from humidity sensor")
+        return 'failed', 'failed'
+        # ("Failed to retrive data from humidity sensor")
--- sender.py
+++ sender.py
@@ -2,5 +2,9 @@ import requests
 import humiditytester
 url = 'http://50.116.54.247:3000/'
 
-obj = {'temp' : humiditytester.temp()}
-x = requests.post(url, obj)
+humid, temp = humiditytester.temp()
+
+if (humid != 0 && temp != 0)  :
+    obj = {'humidity' : humid,
+            'temperature' : temp}
+    x = requests.post(url, obj)
